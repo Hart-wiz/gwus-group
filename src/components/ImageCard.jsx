c3711de10aaca2f69a59225a565b6734f8507c34
njimogu wisdom <njimoguwisdom@gmail.com>
2025-05-26 18:09:51 +0100

    changed imagecard icon size , made it a bit bigger especially in mobile

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -4,8 +4,8 @@ const ImageCard = ({ cardImage, title }) => {
   return (
     <div className="flex">
       <div className="flex flex-col text-center items-center ">
-        <div className="hover:scale-95 hover:shadow-lg hover:shadow-black hover:border-none cursor-pointer  rounded-full bg-gray-300/30 overflow-hidden flex justify-center max-md:w-15">
-          <img src={cardImage} alt="" className="w-40 max-md:w-15" />
+        <div className="hover:scale-95 hover:shadow-lg hover:shadow-black hover:border-none cursor-pointer  rounded-full bg-gray-300/30 overflow-hidden flex justify-center max-md:w-30">
+          <img src={cardImage} alt="" className="w-35" />
         </div>
         <h2 className="font-bold text-2xl text-blue-950 max-md:text-sm ">
           {title}
